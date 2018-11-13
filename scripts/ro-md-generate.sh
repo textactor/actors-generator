83e6d3c1631a3be3b596ef43037d8646ca205a7c
Mitica <canteadumitru@gmail.com>
2018-11-13 17:47:49 +0200

    prod v0.2.0

--- scripts/ro-md-generate.sh
+++ scripts/ro-md-generate.sh
@@ -2,9 +2,7 @@
 
 yarn tsc
 
-PAST_DAYS=4 PERIOD=4 LOCALE=ro-md yarn collect
-PAST_DAYS=7 PERIOD=4 LOCALE=ro-md yarn collect
-PAST_DAYS=10 PERIOD=4 LOCALE=ro-md yarn collect
+LOCALE=ro-md yarn collect
 
 LOCALE=ro-md yarn generate
 
